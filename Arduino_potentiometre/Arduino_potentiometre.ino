b16a3d3d66b07a169ebd360bcc3bb0293a296503
damien <dnicolas@etud.insa-toulouse.fr>
2018-11-08 20:43:56 +0100

    comptage des ouvertures/fermetures OPE

--- Arduino_potentiometre/Arduino_potentiometre.ino
+++ Arduino_potentiometre/Arduino_potentiometre.ino
@@ -1,4 +1,10 @@
+#include "CurieTimerOne.h"
+
+
+
 #define ROTARY_ANGLE_SENSOR A0
+
+#define SORTIE A1;
 
 
 #define ADC_REF 5 //reference voltage of ADC is 5v.If the Vcc switch on the seeeduino
@@ -8,6 +14,16 @@
 
 #define FULL_ANGLE 300 //full value of the rotary angle is 300 degrees
 
+#define GROVE_VCC 5 //VCC of the grove interface is normally 5v
+
+ 
+#define TEMPS_D_OBSERVATION 600 // En seconde
+
+const int uneSecondeEnMicroSec  = 2000000;
+
+int nombre_d_ouverture;
+int secondes;
+bool etat_haut = false;
 
 
 void setup()
@@ -16,7 +32,15 @@ void setup()
 
     Serial.begin(9600);
     pinMode(ROTARY_ANGLE_SENSOR, INPUT);
-  
+ 
+    CurieTimerOne.start(uneSecondeEnMicroSec, &AddSeconde);
+
+    nombre_d_ouverture = 0;
+    secondes = 0;
+    
+
+
+
 }
 
 
@@ -24,16 +48,38 @@ void setup()
 void loop()
 
 {   
+    
+    
+    if(analogRead(ROTARY_ANGLE_SENSOR) > 100 && etat_haut == false )
+    { 
+      nombre_d_ouverture ++;
+      etat_haut = true;
+    }
+    if(analogRead(ROTARY_ANGLE_SENSOR) == 0)
+    {
+      
+      etat_haut = false;
+    }
+    
 
-    float voltage;
-    int sensor_value = analogRead(ROTARY_ANGLE_SENSOR);
-    voltage = (float)sensor_value*ADC_REF/1023;
-    float degrees = (voltage*FULL_ANGLE)/GROVE_VCC;
-    Serial.println("The angle between the mark and the starting position:");
-    Serial.println(degrees);
+    delayMicroseconds(100);
 
-
-
-    delay(500);
+    
+    
+    
+    
 
 }
+
+void AddSeconde(){
+  Serial.print("Bonjour depuis l'interruption :");
+  secondes ++;
+  Serial.println(secondes);
+  if(secondes == 5 ){
+      Serial.print("ça fait 10 secondes, on a eu ");
+      Serial.println(nombre_d_ouverture);
+      nombre_d_ouverture = 0;
+      secondes = 0;
+  }
+}
+
