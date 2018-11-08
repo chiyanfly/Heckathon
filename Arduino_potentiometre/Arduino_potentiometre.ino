73a040a4b830b6b6ca6d618ba2330fee977b251d
damien <dnicolas@etud.insa-toulouse.fr>
2018-11-08 20:48:58 +0100

    comptage des ouvertures/fermetures propre

--- Arduino_potentiometre/Arduino_potentiometre.ino
+++ Arduino_potentiometre/Arduino_potentiometre.ino
@@ -4,22 +4,10 @@
 
 #define ROTARY_ANGLE_SENSOR A0
 
-#define SORTIE A1;
-
-
-#define ADC_REF 5 //reference voltage of ADC is 5v.If the Vcc switch on the seeeduino
-
-                    //board switches to 3V3, the ADC_REF should be 3.3
-
-
-#define FULL_ANGLE 300 //full value of the rotary angle is 300 degrees
-
-#define GROVE_VCC 5 //VCC of the grove interface is normally 5v
-
  
-#define TEMPS_D_OBSERVATION 600 // En seconde
+#define TEMPS_D_OBSERVATION 600 // 10 minutes en secondes
 
-const int uneSecondeEnMicroSec  = 2000000;
+const int uneSecondeEnMicroSec  = 1000000;
 
 int nombre_d_ouverture;
 int secondes;
@@ -72,14 +60,14 @@ void loop()
 }
 
 void AddSeconde(){
-  Serial.print("Bonjour depuis l'interruption :");
+  
   secondes ++;
-  Serial.println(secondes);
-  if(secondes == 5 ){
-      Serial.print("ça fait 10 secondes, on a eu ");
-      Serial.println(nombre_d_ouverture);
+  
+  if(secondes == TEMPS_D_OBSERVATION ){
+      
       nombre_d_ouverture = 0;
       secondes = 0;
+      //Envoie LoRa
   }
 }
 
